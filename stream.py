c969b813d1eed11f88443f902babfc771cf500c8
seweithotroyman <seweithotroyman@gmail.com>
2019-06-26 21:31:40 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -19,6 +19,11 @@ class StdOutListener(StreamListener):
     """ A listener handles tweets that are received from the stream.
     This is a basic listener that just prints received tweets to stdout.
     """
+    
+    #def on_data(self, data):
+    #    print(data)
+    #    return True
+    
     def on_status(self, status):
         print(status.text)
 
